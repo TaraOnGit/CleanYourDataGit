e9fa8c178b63f493b3eb4c571eda823e5caab912
Tara <taranum.g@gmail.com>
2024-07-10 05:20:08 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Clean My Data')
+st.header('Welcome')
+st.subheader('Data Cleaning Made Easy')
+st.write('What do you want to do?')
+st.markdown("""
+- Remove Duplicates
+- Fill Null Values
+- Encode Categorical Data
+""")
