5083822f40204504bc44714f76e10ebc3c4efe1e
Tara <taranum.g@gmail.com>
2024-07-14 23:08:13 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import numpy as np
 import streamlit as st
+import pandas as pd
 
 st.title('Clean My Data')
-st.header('Welcome')
-st.subheader('Data Cleaning Made Easy')
-st.write('What do you want to do?')
-st.markdown("""
-- Remove Duplicates
-- Fill Null Values
-- Encode Categorical Data
-""")
+file = st.file_uploader('Please upload a csv file')
+
+st.sidebar.title('What do you want to do?')
+options = ['Remove Duplicates','Remove Rows with NaN Values']
+opt = st.sidebar.selectbox('From my Data ',options)
+df = pd.DataFrame()
+temp_df = pd.DataFrame()
+if file != None:
+    df = pd.read_csv(file)
+if opt == 'Remove Duplicates' :
+    temp_df = df.drop_duplicates()
+elif opt == 'Remove Rows with NaN Values':
+    temp_df = df.dropna(axis=0)
+
+st.download_button(label='Download CSV File', data=temp_df.to_csv().encode('utf-8'), file_name='CleanedFile.csv')
